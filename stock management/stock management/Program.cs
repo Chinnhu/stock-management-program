d60b13624f166c3615893da2e0594fe65b26f466
Chinnhu <alexangelov.angelov00@gmail.com>
2023-02-01 19:36:54 +0200

    login page thing
    napravo ne moga da povqrvam che izpolzvame tva

--- stock management/stock management/Program.cs
+++ stock management/stock management/Program.cs
@@ -16,7 +16,7 @@ namespace stock_management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
